00a7631522b6adcdfe4e2141d003ab1c67adc89f
JonasLeiserson <49516747@est.ort.edu.ar>
2024-11-26 21:45:23 -0300

    Bueno, ahora tenemos botones

--- SERVEREPICARDO/server.js
+++ SERVEREPICARDO/server.js
@@ -1,8 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const cors = require('cors');
 const PORT = 3000;
+app.use(express.json());
+app.use(cors());
+console.log("Servidor Express arrancando...");
+
+
+
+
+let puntaje = 0
+
 
 app.get('/puntaje', (req, res) => {
-    res.json({ mensaje: 'Solicitud GET recibida', data: [1, 2, 3] });
-});
+    puntaje = puntaje + 1
+    res.json({ puntaje: puntaje });  
+  });
+  app.get('/ActualizarPuntaje', (req, res) => {
+    res.json({ puntaje: puntaje });  
+  });
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+  });
